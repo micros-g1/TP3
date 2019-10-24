a7bfab8849c2db98cfa8e3bb2347c8c371888258
taomasgonzalez <togonzalez@itba.edu.ar>
2019-10-24 16:46:39 -0300

    minor nomenclature changes

--- source/FTM/flex_timer.c
+++ source/FTM/flex_timer.c
@@ -17,7 +17,7 @@
 /*****************************************************
  * *****************STATIC VARIABLES******************
  *****************************************************/
-static FTM_Type * FTMs[FTM_AMOUNT_MODULES] = FTM_BASE_PTRS;
+static FTM_Type * ftms[FTM_AMOUNT_MODULES] = FTM_BASE_PTRS;
 static ftm_irq_callback_t irq_callbacks[FTM_AMOUNT_MODULES][FTM_AMOUNT_CHANNELS];
 //for pwm configurations
 static uint32_t const pwm_combine_masks[4]= {FTM_COMBINE_COMBINE0_MASK, FTM_COMBINE_COMBINE1_MASK,
@@ -63,28 +63,28 @@ void ftm_init(ftm_modules_t module, ftm_prescaler_t prescaler_config){
 
 // CONTROL STATUS REGISTER		SC
 void ftm_start_stop_clock(ftm_modules_t module, bool start_stop){
-	FTMs[module]->SC = (FTMs[module]->SC & ~FTM_SC_CLKS_MASK) | FTM_SC_CLKS(start_stop);
+	ftms[module]->SC = (ftms[module]->SC & ~FTM_SC_CLKS_MASK) | FTM_SC_CLKS(start_stop);
 }
 void ftm_set_prescaler(ftm_modules_t module , ftm_prescaler_t prescaler_config){
-	FTMs[module]->SC = (FTMs[module]->SC & ~FTM_SC_PS_MASK) | FTM_SC_PS(prescaler_config);
+	ftms[module]->SC = (ftms[module]->SC & ~FTM_SC_PS_MASK) | FTM_SC_PS(prescaler_config);
 }
 void ftm_enable_overflow_interrupts(ftm_modules_t module, bool enable_disable){
-	FTMs[module]->SC = (FTMs[module]->SC & ~FTM_SC_TOIE_MASK) | FTM_SC_TOIE(enable_disable);
+	ftms[module]->SC = (ftms[module]->SC & ~FTM_SC_TOIE_MASK) | FTM_SC_TOIE(enable_disable);
 }
 void ftm_up_up_downcpwms(ftm_modules_t module, bool up_up_down){
-	FTMs[module]->SC = (FTMs[module]->SC & ~FTM_SC_CPWMS_MASK) | FTM_SC_CPWMS(up_up_down);
+	ftms[module]->SC = (ftms[module]->SC & ~FTM_SC_CPWMS_MASK) | FTM_SC_CPWMS(up_up_down);
 }
 bool ftm_has_overflowed(ftm_modules_t module){
-	return FTMs[module]->SC & FTM_SC_TOF_MASK;
+	return ftms[module]->SC & FTM_SC_TOF_MASK;
 }
 
 // COUNTER REGISTER				CNT
 void ftm_reset_counter_value(ftm_modules_t module){
 	//Reset clears the CNT register. Writing any value to COUNT updates the counter with its initial value, CNTIN.
-	FTMs[module]->CNT |= FTM_CNT_COUNT(0x01);
+	ftms[module]->CNT |= FTM_CNT_COUNT(0x01);
 }
 uint16_t ftm_read_counter_value(ftm_modules_t module){
-	return FTMs[module]->CNT & FTM_CNT_COUNT_MASK;
+	return ftms[module]->CNT & FTM_CNT_COUNT_MASK;
 }
 
 //MOD REGISTER					MOD
@@ -98,32 +98,32 @@ avoid confusion about when the first counter overflow will occur.
  */
 
 void write_mod_value(ftm_modules_t module , uint16_t mod_value){
-	FTMs[module]->MOD = mod_value;
+	ftms[module]->MOD = mod_value;
 }
 uint16_t ftm_get_mod_value(ftm_modules_t module){
-	return FTMs[module]->MOD;
+	return ftms[module]->MOD;
 }
 
 //Channel (n) Status And Control		CnSC
 
 void ftm_enable_dma(ftm_modules_t module, bool enable_disable){
-//	FTMs[module]->CONTROLS->CnSC;
+//	ftms[module]->CONTROLS->CnSC;
 }
 
 
 void ftm_set_pwm_conf(ftm_modules_t module, ftm_pwm_config_t config){
 
 	if(config.enable_dma)
-		FTMs[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_DMA_MASK|FTM_CnSC_CHIE_MASK;		//enables dma
+		ftms[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_DMA_MASK|FTM_CnSC_CHIE_MASK;		//enables dma
 	else
-		(FTMs[module]->CONTROLS[config.channel].CnSC) &= ~FTM_CnSC_DMA_MASK;						//disables dma
+		(ftms[module]->CONTROLS[config.channel].CnSC) &= ~FTM_CnSC_DMA_MASK;						//disables dma
 
 	/*	SWSOC
 	* Software output control synchronization is activated by the software trigger.
 	*	0 The software trigger does not activate the SWOCTRL register synchronization.
 	*	1 The software trigger activates the SWOCTRL register synchronization.
 	*/
-	FTMs[module]->SYNCONF |= FTM_SYNCONF_SWWRBUF_MASK | FTM_SYNCONF_SWSOC_MASK;
+	ftms[module]->SYNCONF |= FTM_SYNCONF_SWWRBUF_MASK | FTM_SYNCONF_SWSOC_MASK;
 	/*
 	 * PWM Synchronization Software Trigger
 	 * Selects the software trigger as the PWM synchronization trigger. The software trigger happens when a 1 is
@@ -131,55 +131,55 @@ void ftm_set_pwm_conf(ftm_modules_t module, ftm_pwm_config_t config){
 	 * 0 Software trigger is not selected.
 	 * 1 Software trigger is selected.
 	 */
-	FTMs[module]->SYNC |= FTM_SYNC_SWSYNC_MASK;
+	ftms[module]->SYNC |= FTM_SYNC_SWSYNC_MASK;
 
 	// QUADEN = 0
-	FTMs[module]->QDCTRL &= ~FTM_QDCTRL_QUADEN_MASK;
+	ftms[module]->QDCTRL &= ~FTM_QDCTRL_QUADEN_MASK;
 
 
 
-	FTMs[module]->COMBINE &= ~(pwm_decapen_masks[module]|pwm_combine_masks[module]);
+	ftms[module]->COMBINE &= ~(pwm_decapen_masks[module]|pwm_combine_masks[module]);
 
 
 	uint32_t SYNCEN_MASK[]= {FTM_COMBINE_SYNCEN0_MASK, FTM_COMBINE_SYNCEN1_MASK,
 			FTM_COMBINE_SYNCEN2_MASK, FTM_COMBINE_SYNCEN3_MASK};
-	FTMs[module]->COMBINE|=SYNCEN_MASK[module];
+	ftms[module]->COMBINE|=SYNCEN_MASK[module];
 
 	if(config.mode == FTM_PWM_CENTER_ALIGNED){
-		FTMs[module]->CONTROLS[config.channel].CnSC|=FTM_CnSC_ELSB_MASK;
-		FTMs[module]->SC |= FTM_SC_CPWMS_MASK;		//counting up and down
+		ftms[module]->CONTROLS[config.channel].CnSC|=FTM_CnSC_ELSB_MASK;
+		ftms[module]->SC |= FTM_SC_CPWMS_MASK;		//counting up and down
 	}
 	else if(config.mode == FTM_PWM_EDGE_ALIGNED){
-		FTMs[module]->SC &= ~FTM_SC_CPWMS_MASK;			//centered PWM disabled.
+		ftms[module]->SC &= ~FTM_SC_CPWMS_MASK;			//centered PWM disabled.
 		// edge alligned, high true pulses
-		FTMs[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK;
+		ftms[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK;
 	}
 
 	write_mod_value(module , config.mod);
-	FTMs[module]->CONTROLS[config.channel].CnV=config.CnV;
-	FTMs[module]->CNTIN = 0;		//resets counter value.
+	ftms[module]->CONTROLS[config.channel].CnV=config.CnV;
+	ftms[module]->CNTIN = 0;		//resets counter value.
 }
 
 void ftm_set_input_capture_conf(ftm_modules_t module, ftm_input_capture_config_t config){
 
-	FTMs[module]->CONTROLS[config.channel].CnSC= FTM_CnSC_ELSA(config.mode & 1)|FTM_CnSC_ELSB((config.mode << 1 )&1);
+	ftms[module]->CONTROLS[config.channel].CnSC= FTM_CnSC_ELSA(config.mode & 1)|FTM_CnSC_ELSB((config.mode << 1 )&1);
 
 	if(config.channel <= 3){
 		uint32_t filters_masks[4]={FTM_FILTER_CH0FVAL(config.filter_value), FTM_FILTER_CH1FVAL(config.filter_value),
 				FTM_FILTER_CH2FVAL(config.filter_value), FTM_FILTER_CH3FVAL(config.filter_value)};
-		FTMs[module]->FILTER|=filters_masks[config.channel];
+		ftms[module]->FILTER|=filters_masks[config.channel];
 	}
 	//CALLBACK
 	irq_callbacks[module][config.channel] = config.callback;
 
 	if(config.enable_dma)
-		FTMs[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_DMA_MASK|FTM_CnSC_CHIE_MASK;
+		ftms[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_DMA_MASK|FTM_CnSC_CHIE_MASK;
 	else{
-		(FTMs[module]->CONTROLS[config.channel].CnSC) &= ~FTM_CnSC_DMA_MASK;
-		FTMs[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_CHIE_MASK;
+		(ftms[module]->CONTROLS[config.channel].CnSC) &= ~FTM_CnSC_DMA_MASK;
+		ftms[module]->CONTROLS[config.channel].CnSC |= FTM_CnSC_CHIE_MASK;
 	}
 
-	FTMs[module]->CNTIN=0;		//resets the counter
+	ftms[module]->CNTIN=0;		//resets the counter
 	write_mod_value(module, config.mod);
 }
 
